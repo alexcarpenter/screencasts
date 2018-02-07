3286f66ac2f593115770ced78b6f0afcab54db2d
Alex Carpenter <alex@mightyinthemidwest.com>
2018-02-07 09:13:27 -0500

    Fix vscode indentation

--- vscode.user.js
+++ vscode.user.js
@@ -1,44 +1,44 @@
 {
-  "editor.fontSize": 16,
+    "editor.fontSize": 16,
     "editor.fontFamily": "Input Mono, monospace",
-      "editor.lineHeight": 45,
-        "workbench.colorTheme": "Palenight Theme",
-          "editor.tabSize": 2,
-            "workbench.activityBar.visible": false,
-              "workbench.statusBar.visible": true,
-                "workbench.sideBar.location": "right",
-                  "editor.wordWrap": "off",
-                    "editor.minimap.enabled": false,
-                      "emmet.triggerExpansionOnTab": true,
-                        "editor.selectionHighlight": false,
-                          "emmet.showAbbreviationSuggestions": false,
-                            "editor.renderLineHighlight": "gutter",
-                              "workbench.startupEditor": "newUntitledFile",
-                                "editor.renderWhitespace": "all",
-                                  "files.exclude": {
-    "**/.git": true,
-      "**/.svn": true,
+    "editor.lineHeight": 45,
+    "workbench.colorTheme": "Palenight Theme",
+    "editor.tabSize": 2,
+    "workbench.activityBar.visible": false,
+    "workbench.statusBar.visible": true,
+    "workbench.sideBar.location": "right",
+    "editor.wordWrap": "off",
+    "editor.minimap.enabled": false,
+    "emmet.triggerExpansionOnTab": true,
+    "editor.selectionHighlight": false,
+    "emmet.showAbbreviationSuggestions": false,
+    "editor.renderLineHighlight": "gutter",
+    "workbench.startupEditor": "newUntitledFile",
+    "editor.renderWhitespace": "all",
+    "files.exclude": {
+        "**/.git": true,
+        "**/.svn": true,
         "**/.hg": true,
-          "**/CVS": true,
-            "**/.DS_Store": true,
-              "**/node_modules": true
-  },
-  "workbench.panel.location": "bottom",
-    "vim.startInInsertMode": true,
-      "vim.insertModeKeyBindings": [
-        {
-          "before": ["j", "j"],
-          "after": ["<Esc>"]
-        }
-      ],
-        "files.associations": {
-    "*.html": "twig"
-  },
-  "emmet.includeLanguages": {
-    "twig": "html",
+        "**/CVS": true,
+        "**/.DS_Store": true,
+        "**/node_modules": true
     },
-  "window.zoomLevel": 1,
+    "workbench.panel.location": "bottom",
+    "vim.startInInsertMode": true,
+    "vim.insertModeKeyBindings": [
+        {
+            "before": ["j","j"],
+            "after": ["<Esc>"]
+        }
+    ],
+    "files.associations": {
+        "*.html": "twig"
+    },
+    "emmet.includeLanguages": {
+        "twig":"html",
+    },
+    "window.zoomLevel": 1,
     "[javascript]": {
-    "editor.formatOnSave": true
-  }
+        "editor.formatOnSave": true
+    }
 }
